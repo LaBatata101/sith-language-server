3f0937cec32c8e74ad50f0f0d15464e8a76b4858
LaBatata101 <labatata101@linuxmail.org>
2023-10-22 21:50:38 -0300

    Misc: add `Ranged` trait

--- crates/ruff_text_size/src/lib.rs
+++ crates/ruff_text_size/src/lib.rs
@@ -23,7 +23,7 @@ mod range;
 mod size;
 mod traits;
 
-pub use crate::{range::TextRange, size::TextSize, traits::TextLen};
+pub use crate::{range::TextRange, size::TextSize, traits::Ranged, traits::TextLen};
 
 #[cfg(target_pointer_width = "16")]
 compile_error!("text-size assumes usize >= u32 and does not work on 16-bit targets");
