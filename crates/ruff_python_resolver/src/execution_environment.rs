a539ab594b5717854e7a810686693fd41fe14b34
LaBatata101 <labatata101@gmail.com>
2024-08-18 18:26:37 -0300

    Add Python import resolver

---
+++ crates/ruff_python_resolver/src/execution_environment.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+use crate::python_platform::PythonPlatform;
+use crate::python_version::PythonVersion;
+
+#[derive(Debug, Clone)]
+pub struct ExecutionEnvironment {
+    /// The root directory of the execution environment.
+    pub root: PathBuf,
+
+    /// The Python version of the execution environment.
+    pub python_version: PythonVersion,
+
+    /// The Python platform of the execution environment.
+    pub python_platform: PythonPlatform,
+
+    /// The extra search paths of the execution environment.
+    pub extra_paths: Vec<PathBuf>,
+}
